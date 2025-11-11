05d134c0281584d05f42c2e134eb1438e18411de
talha <baigtalha7@gmail.com>
2025-11-11 11:51:52 +0500

    Basic App layout setup, removed un-necessary default styles and pages

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-app>
+    <AppHeader />
+    
+    <v-main>
+      <router-view />
+    </v-main>
+    
+    <AppFooter />
+  </v-app>
 </template>
 
 <style scoped>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,26 +1,18 @@
 import { createStore } from 'vuex';
 
 export interface State {
-  count: number;
+
 }
 
 export default createStore<State>({
   state: {
-    count: 0,
   },
   mutations: {
-    increment(state) {
-      state.count++;
-    },
+   
   },
   actions: {
-    increment({ commit }) {
-      commit('increment');
-    },
+   
   },
   getters: {
-    getCount(state) {
-      return state.count;
-    },
   },
 });
